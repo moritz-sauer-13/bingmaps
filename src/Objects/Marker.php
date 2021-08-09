5f4576c6b237b4a3c27f2232c9bc8e99a3663592
Moritz Sauer <43135946+moritz-sauer-13@users.noreply.github.com>
2021-08-09 14:58:54 +0200

    Update Marker.php

--- src/Objects/Marker.php
+++ src/Objects/Marker.php
@@ -4,7 +4,7 @@ namespace bingMap;
 // Corresponds to BING Pushpin
 class Marker
 {
-    use MapPosition;
+    //use MapPosition;
     private $ID;
     private $InfoBox = null;
     private $IconPath = null;
@@ -107,4 +107,49 @@ class Marker
         }
         return $data;
     }
+    
+        protected $Coords;
+
+    public function SetPosition($coords)
+    {
+        $this->Coords = $coords;
+        return $this;
+    }
+    public function GetPosition()
+    {
+        return $this->Coords;
+    }
+    public function GetLatitude()
+    {
+        return $this->Coords->GetLatitude();
+    }
+    public function GetLongitude()
+    {
+        return $this->Coords->GetLongitude();
+    }
+    public function GetLocationVariable($ID,$Suffix)
+    {
+        return "Location_{$Suffix}_{$ID}";
+    }
+    public function HasPosition()
+    {
+        return $this->Coords != null;
+    }
+    //Might Rename to IsValidCoordinate could cause misunderstanding
+    public function IsValidCoordinate()
+    {
+        return $this->Coords->IsValid();
+    }
+    public function RenderLocationVariable($ID,$Suffix)
+    {
+        return "var Location_{$Suffix}_{$ID} = new Microsoft.Maps.Location({$this->GetLatitude()},{$this->GetLongitude()}); ";
+    }
+    public function RenderLocation()
+    {
+        if($this->HasPosition())
+        {
+            return "new Microsoft.Maps.Location({$this->GetLatitude()},{$this->GetLongitude()})";
+        }
+        return "";
+    } 
 }
