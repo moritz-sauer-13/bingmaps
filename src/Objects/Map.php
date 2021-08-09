c03b729ecb35d829279769170c318bab3461da9e
Moritz Sauer <43135946+moritz-sauer-13@users.noreply.github.com>
2021-08-09 14:59:45 +0200

    Update Map.php

--- src/Objects/Map.php
+++ src/Objects/Map.php
@@ -1,13 +1,13 @@
 <?php
 namespace bingMap;
 
-use bingMap\MapPosition;
-use SilverStripe\Dev\Debug;
-use SilverStripe\View\ViewableData;
+//use bingMap\MapPosition;
+//use SilverStripe\Dev\Debug;
+//use SilverStripe\View\ViewableData;
 
 class Map extends ViewableData
 {
-    use MapPosition;
+    //use MapPosition;
 
     private $Debug;
     private $ID;
@@ -218,5 +218,50 @@ class Map extends ViewableData
     {
         return json_encode($this->GetReactData());
     }
+    
+        protected $Coords;
+
+    public function SetPosition($coords)
+    {
+        $this->Coords = $coords;
+        return $this;
+    }
+    public function GetPosition()
+    {
+        return $this->Coords;
+    }
+    public function GetLatitude()
+    {
+        return $this->Coords->GetLatitude();
+    }
+    public function GetLongitude()
+    {
+        return $this->Coords->GetLongitude();
+    }
+    public function GetLocationVariable($ID,$Suffix)
+    {
+        return "Location_{$Suffix}_{$ID}";
+    }
+    public function HasPosition()
+    {
+        return $this->Coords != null;
+    }
+    //Might Rename to IsValidCoordinate could cause misunderstanding
+    public function IsValidCoordinate()
+    {
+        return $this->Coords->IsValid();
+    }
+    public function RenderLocationVariable($ID,$Suffix)
+    {
+        return "var Location_{$Suffix}_{$ID} = new Microsoft.Maps.Location({$this->GetLatitude()},{$this->GetLongitude()}); ";
+    }
+    public function RenderLocation()
+    {
+        if($this->HasPosition())
+        {
+            return "new Microsoft.Maps.Location({$this->GetLatitude()},{$this->GetLongitude()})";
+        }
+        return "";
+    } 
 
 }
