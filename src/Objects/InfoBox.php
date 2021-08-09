985343f1ed2cacba05817a6ed15fad3fe85b8f95
Moritz Sauer <43135946+moritz-sauer-13@users.noreply.github.com>
2021-08-09 15:00:53 +0200

    Update InfoBox.php

--- src/Objects/InfoBox.php
+++ src/Objects/InfoBox.php
@@ -2,13 +2,13 @@
 
 namespace bingMap;
 
-use bingMap\MapPosition;
-use bingMap\HelperMethods;
+//use bingMap\MapPosition;
+//use bingMap\HelperMethods;
 use SilverStripe\View\ViewableData;
 
 class InfoBox
 {
-    use MapPosition;
+    //use MapPosition;
     
     private $ID;
     private $Title = null;
@@ -157,4 +157,49 @@ class InfoBox
             "coordinates"   => $this->GetPosition()->GetReactData()
         ];
     }
+    
+        protected $Coords;
+
+    public function SetPosition($coords)
+    {
+        $this->Coords = $coords;
+        return $this;
+    }
+    public function GetPosition()
+    {
+        return $this->Coords;
+    }
+    public function GetLatitude()
+    {
+        return $this->Coords->GetLatitude();
+    }
+    public function GetLongitude()
+    {
+        return $this->Coords->GetLongitude();
+    }
+    public function GetLocationVariable($ID,$Suffix)
+    {
+        return "Location_{$Suffix}_{$ID}";
+    }
+    public function HasPosition()
+    {
+        return $this->Coords != null;
+    }
+    //Might Rename to IsValidCoordinate could cause misunderstanding
+    public function IsValidCoordinate()
+    {
+        return $this->Coords->IsValid();
+    }
+    public function RenderLocationVariable($ID,$Suffix)
+    {
+        return "var Location_{$Suffix}_{$ID} = new Microsoft.Maps.Location({$this->GetLatitude()},{$this->GetLongitude()}); ";
+    }
+    public function RenderLocation()
+    {
+        if($this->HasPosition())
+        {
+            return "new Microsoft.Maps.Location({$this->GetLatitude()},{$this->GetLongitude()})";
+        }
+        return "";
+    } 
 }
